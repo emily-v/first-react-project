f8152e27f5d2ebe584fd366a1d5aa59aa4a5db7f
Emily Vong <emily.vong25@gmail.com>
2018-06-13 22:51:37 +0000

    duplicated card component

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,13 @@ ReactDOM.render(
     <div>
         <Navbar />
         <PricingHeader />
-        <PricingCards />
+        <div className="container card-deck-container">
+            <div className="card-deck">
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
         <Footer />
     </div>
     ,
@@ -48,59 +54,59 @@ function PricingHeader(){
     </div>;
 }
 
-function PricingCards(){
-    return <div>
-        <div className="container card-deck-container">
-            <div className="card-deck">
-                <div className="card mb-4 box-shadow">
-                    <div className="card-header">
-                        <h4>Free</h4>
-                    </div>
-                    <div className="card-body">
-                        <h1 className="card-title">&#36;0 <small className="text-muted">&#47; mo</small></h1>
-                        <ul className="list-unstyled mt-3 mb-4">
-                            <li>10 users included</li>
-                            <li>2 GB of storage</li>
-                            <li>Email support</li>
-                            <li>Help center access</li>
-                        </ul>
-                        <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
-                    </div>
-                </div>
-                <div className="card mb-4 box-shadow">
-                    <div className="card-header">
-                        <h4>Pro</h4>
-                    </div>
-                    <div className="card-body">
-                        <h1 className="card-title">&#36;15 <small className="text-muted">&#47; mo</small></h1>
-                        <ul className="list-unstyled mt-3 mb-4">
-                            <li>20 users included</li>
-                            <li>10 GB of storage</li>
-                            <li>Priority email support</li>
-                            <li>Help center access</li>
-                        </ul>
-                        <button type="button" className="btn btn-lg btn-block btn-primary">Get started</button>
-                    </div>
-                </div>
-                <div className="card mb-4 box-shadow">
-                    <div className="card-header">
-                        <h4>Enterprise</h4>
-                    </div>
-                    <div className="card-body">
-                        <h1 className="card-title">&#36;29 <small className="text-muted">&#47; mo</small></h1>
-                        <ul className="list-unstyled mt-3 mb-4">
-                            <li>30 users included</li>
-                            <li>15 GB of storage</li>
-                            <li>Phone and email support</li>
-                            <li>Help center access</li>
-                        </ul>
-                        <button type="button" className="btn btn-lg btn-block btn-primary">Contact us</button>
-                    </div>
-                </div>
-            </div>
+function Card(){
+    return <div className="card mb-4 box-shadow">
+        <div className="card-header">
+            <h4 className="text-center">Free</h4>
+        </div>
+        <div className="card-body">
+            <h1 className="card-title">&#36;0 <small className="text-muted">&#47; mo</small></h1>
+            <ul className="list-unstyled mt-3 mb-4">
+                <li>10 users included</li>
+                <li>2 GB of storage</li>
+                <li>Email support</li>
+                <li>Help center access</li>
+            </ul>
+            <button type="button" className="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
         </div>
     </div>;
 }
+        
+/*function Pro(){
+    return <div className="card mb-4 box-shadow">
+        <div className="card-header">
+            <h4>Pro</h4>
+        </div>
+        <div className="card-body">
+            <h1 className="card-title">&#36;15 <small className="text-muted">&#47; mo</small></h1>
+            <ul className="list-unstyled mt-3 mb-4">
+                <li>20 users included</li>
+                <li>10 GB of storage</li>
+                <li>Priority email support</li>
+                <li>Help center access</li>
+            </ul>
+            <button type="button" className="btn btn-lg btn-block btn-primary">Get started</button>
+        </div>
+    </div>;
+}
+
+function Enterprise(){
+    return <div className="card mb-4 box-shadow">
+        <div className="card-header">
+            <h4>Enterprise</h4>
+        </div>
+        <div className="card-body">
+            <h1 className="card-title">&#36;29 <small className="text-muted">&#47; mo</small></h1>
+            <ul className="list-unstyled mt-3 mb-4">
+                <li>30 users included</li>
+                <li>15 GB of storage</li>
+                <li>Phone and email support</li>
+                <li>Help center access</li>
+            </ul>
+            <button type="button" className="btn btn-lg btn-block btn-primary">Contact us</button>
+        </div>
+    </div>;
+}*/
 
 function Footer(props){
     return (<footer className="pt-4 my-md-5 pt-md-5 border-top mx-auto">
